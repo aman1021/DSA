3896a9a394ae08a931decfe997e67579570be15d
Aman Chandel <amaniiitu32@gmail.com>
2021-12-24 22:53:03 +0530

    BinaryTrees

---
+++ BinaryTrees/HeightofBinaryTree.cpp
@@ -0,0 +1,11 @@
+   //USING RECURSION
+   
+    int maxDepth(TreeNode* root) {
+        if(root == NULL){
+            return 0;
+        }
+        int leftHeight = maxDepth(root->left);
+        int rightHeight = maxDepth(root->right);
+        
+        return 1+ max(leftHeight, rightHeight);
+    }
