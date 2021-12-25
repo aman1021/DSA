8983ddae23766252898f6ec726c0f888ccb1878d
Aman Chandel <amaniiitu32@gmail.com>
2021-12-25 22:32:24 +0530

    string

--- Strings/reverseWords.cpp
+++ Strings/reverseWords.cpp
@@ -5,27 +5,42 @@ using namespace std;
 //OUTPUT = blue is sky the
 
 string reverseWords(string s){
-          vector<string> temp;
-          
-          for(int i =0; i < s.length(); i++){
-              string currStr;
-              while(s[i] != ' ' && i<s.length()){
-                  currStr+=s[i]; //Concatenating every character of a particular word of string s in currrstr and pushback in vector temp.
-                  i++;
-              }
-              temp.push_back(currStr);
-              if(i < s.length()){
-                  temp.push_back(" ");
-              }
-          }
-
-          string ans = "";
-          for(int i = 0; i< temp.size(); i++){
-                  ans+=temp[temp.size() - i - 1]; //Concatenating every word and space in vector temp with string ans 
-                                                   //and returning the ans as reverse words.
-          }
-
-          return ans;
+      	//we make an answer string, that we have to return
+        string ans = "";
+		
+	//variable for traversing the input string
+        int i = 0;
+	
+	//here we are traversing the input string
+        while(i<s.size()){
+		//this is the string to store the individual words
+            string w;
+			//when the spaces occur keep on going further
+            while(s[i] == ' '){
+                i++;
+            }
+            
+			//as we detect a char we start putting into our w string for storing the word
+			//this will keep going on until we detect a space
+            while(i<s.size() and s[i]!=' '){
+                w.push_back(s[i]);
+                i++;
+            }
+			
+			// i was stopped on the sight of space so now time to increment i from the current space index
+			//this can be neglected as well because we are taking care of the spaces before the starting of the loop
+            i++;
+            
+			//if the word is empty then do nothing, otherwise attatch it to the front of actual ans string with a space
+            if(w.empty() == false){
+                ans = w + " " + ans;
+            }
+        }
+        
+		//this is a pop to pop the space at the end of the ans, because we are inserting w + space + ans
+		//so when the ans was empty then space was inserted before the word so we need to take care of it.
+        ans.pop_back();
+        return ans;
 }
 int main(){
     string s;
